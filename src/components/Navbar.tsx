416f375827e58d0b278d344417ff68d0e8cf5626
Martyna Michalska <m.michalska4126@gmail.com>
2023-08-13 14:04:20 +0200

    feat: change navbar bg on scroll

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import React from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import {montserrat} from "../styles/fonts";
 
 const Navbar = () => {
@@ -9,8 +9,30 @@ const Navbar = () => {
     {name: "contact", link: ""},
   ];
 
+  const [scrollY, setScrollY] = useState(0);
+  const [isOnTop, setIsOnTop] = useState(false);
+
+  const handleScroll = useCallback(() => {
+    setScrollY(window.scrollY);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [handleScroll]);
+
+  useEffect(() => {
+    setIsOnTop(scrollY < 100);
+  }, [scrollY]);
+
   return (
-    <div className="py-8 sticky top-0 left-0">
+    <div
+      className={`py-8 sticky top-0 left-0 transition-all duration-75 + ${
+        isOnTop ? "" : "bg-[#141414]"
+      }`}
+    >
       <nav className="flex gap-12 container mx-auto justify-end">
         {navLinks.map(({name, link}, idx) => (
           <Link
