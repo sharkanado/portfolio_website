3e647764d45a3e8d4ba8817490aed6cb97b49633
Martyna Michalska <m.michalska4126@gmail.com>
2023-08-14 15:34:34 +0200

    feat: add mobile menu layout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import Link from "next/link";
+import React, {useState} from "react";
 import {montserrat} from "@/src/styles/fonts";
+import {FaBars} from "react-icons/fa";
 
 const Navbar = () => {
   const navLinks = [
@@ -8,23 +8,35 @@ const Navbar = () => {
     {name: "about", link: "#about"},
     {name: "contact", link: "#contact"},
   ];
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  console.log(isMenuOpen);
 
   return (
-    <div
-      className="hidden sm:block py-8 sticky top-0 left-0 transition-all 
+    <div>
+      <div
+        className="hidden md:block py-8 sticky top-0 left-0 transition-all 
        bg-opacity-50 bg-black"
-    >
-      <nav className="flex gap-12 px-5 container mx-auto justify-end">
-        {navLinks.map(({name, link}, idx) => (
-          <Link
-            key={idx}
-            href={link}
-            className={`${montserrat.className} text-xl uppercase hover:text-pink transition-all`}
-          >
-            {name}
-          </Link>
-        ))}
-      </nav>
+      >
+        <nav className="flex gap-12 px-5 container mx-auto justify-end">
+          {navLinks.map(({name, link}, idx) => (
+            <a
+              key={idx}
+              href={link}
+              className={`${montserrat.className} text-xl uppercase hover:text-pink transition-all`}
+            >
+              {name}
+            </a>
+          ))}
+        </nav>
+      </div>
+      <div className="md:hidden fixed flex w-full justify-end top-0 left-0 p-5 bg-opacity-50 bg-black">
+        <button
+          className="btn btn-ghost rounded-full btn-square"
+          onClick={() => setIsMenuOpen((prev) => !prev)}
+        >
+          <FaBars size={30} />
+        </button>
+      </div>
     </div>
   );
 };
