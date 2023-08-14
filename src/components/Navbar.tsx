fa95c4f817a315103667555a08b74473d471317a
Martyna Michalska <m.michalska4126@gmail.com>
2023-08-14 15:19:51 +0200

    fix: remove scroll detection

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect, useState} from "react";
+import React from "react";
 import Link from "next/link";
 import {montserrat} from "@/src/styles/fonts";
 
@@ -9,29 +9,10 @@ const Navbar = () => {
     {name: "contact", link: "#contact"},
   ];
 
-  const [scrollY, setScrollY] = useState(0);
-  const [isOnTop, setIsOnTop] = useState(false);
-
-  const handleScroll = useCallback(() => {
-    setScrollY(window.scrollY);
-  }, []);
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [handleScroll]);
-
-  useEffect(() => {
-    setIsOnTop(scrollY < 100);
-  }, [scrollY]);
-
   return (
     <div
-      className={`hidden sm:block py-8 sticky top-0 left-0 transition-all ${
-        isOnTop ? "" : "bg-opacity-50 bg-black"
-      }`}
+      className="hidden sm:block py-8 sticky top-0 left-0 transition-all 
+       bg-opacity-50 bg-black"
     >
       <nav className="flex gap-12 px-5 container mx-auto justify-end">
         {navLinks.map(({name, link}, idx) => (
